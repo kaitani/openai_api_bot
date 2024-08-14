494e04887166cca99862634b50cdb7772a8a42f0
kaitani <kaitani39@gmail.com>
2024-08-14 19:10:13 +0900

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
+from openai import AsyncOpenAI
+from openai import OpenAI
+import os
+from selenium.common.exceptions import NoSuchElementException
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import time
+from enum import Enum
+import requests
 
+import os  # OpenAI ChatCompletionのAPIキー
+# from openai import AsyncOpenAI
 import streamlit as st
-import openai
+
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+from pathlib import Path
+from llama_index import download_loader
+
+PDFReader = download_loader("PDFReader")
+
+loader = PDFReader()
+documents = loader.load_data(file=Path("/Users/kaiya/Library/CloudStorage/Dropbox/0python/py開発用/chatGPT_LINEbot_GCP/コンプライアンスのすべて.pdf"))
+
+from llama_index import ServiceContext, LLMPredictor, VectorStoreIndex
+from langchain.chat_models import ChatOpenAI
+
+service_context = ServiceContext.from_defaults(
+    llm_predictor=LLMPredictor(
+        llm=ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0)
+    )
+)
+
+# indexを作成
+index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+index.storage_context.persist(persist_dir="./storage/")
+query_engine = index.as_query_engine()
+
+
+
+
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
@@ -18,9 +57,8 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
+    response = query_engine.query(
+    messages=messages
     )
 
     bot_message = response["choices"][0]["message"]
